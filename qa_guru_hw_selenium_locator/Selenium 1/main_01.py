804f0fee45778b0de9d3cf473ceda3fed3bb3d49
lyapin <lyapin@signatec.ru>
2026-07-15 17:20:51 +0700

    move

--- qa_guru_hw_selenium_locator/Selenium 1/main_01.py
+++
@@ -1,7 +0,0 @@
-def main(name):
-    print("Hello, World!")
-    print(f"Hello, {name}!")
-
-# This block ensures main() only runs if the script is executed directly
-if __name__ == "__main__":
-    main("Anton")
